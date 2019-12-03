f36fb18a6b41b9161ea891545914b9a3abee39b3
pimpek32 <37272388+pimpek32@users.noreply.github.com>
2019-12-03 14:01:42 +0100

    huj
    do dupy

---
+++ RECURSE/Assets/Scripts/Gun.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Gun : MonoBehaviour
+{
+    public Animator anim;
+    [SerializeField] public Text ammotext;
+    public float CoolDown = 1f;
+    [HideInInspector]
+     public float cdown;
+    public int curAmmo;
+    public bool reloading;
+    [SerializeField] public float reloadTime;
+    [SerializeField] public int maxAmmo;
+    [SerializeField] public Transform muzzleobject;
+    [SerializeField] public GameObject muzzle;
+    [SerializeField] public GameObject cam;
+    [SerializeField] public float recoil;
+    private float reffloat = 0f;
+        [SerializeField] public float smoothBack;
+    public Quaternion last;
+    public RaycastHit hit;
+    [SerializeField] AudioClip shoot;
+    public AudioSource gunSound;
+    public Transform rand;
+    private void Start()
+    {
+        cdown = CoolDown;
+        cam = GameObject.Find("Game_Camera");
+        gunSound = GetComponent<AudioSource>();
+        rand = transform.Find("Rand");
+        curAmmo = maxAmmo;
+        ammotext = GameObject.Find("ammotext").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ammotext.text = curAmmo.ToString();
+        if (Input.GetButtonDown("Fire1") && CoolDown < 0 && curAmmo > 0 && !reloading)
+        {
+            Shoot();
+        }
+        if(curAmmo == 0 && !reloading)
+        {
+            StartCoroutine(Reload());
+        }
+        cam.GetComponent<MouseLook>().originalRotation = Quaternion.Lerp(cam.GetComponent<MouseLook>().originalRotation, last, smoothBack);
+        rand.localRotation = Quaternion.Lerp(rand.localRotation, Quaternion.Euler(0,0,0), smoothBack);
+        CoolDown -= Time.deltaTime;
+    }
+
+    public void Shoot()
+    {
+        curAmmo--;
+        gunSound.pitch = Random.Range(0.9f, 1.1f);
+        gunSound.PlayOneShot(shoot);
+        CoolDown = cdown;
+        anim.SetTrigger("shoot");
+        GameObject muz = Instantiate(muzzle, muzzleobject.position, muzzleobject.rotation);
+        muz.transform.parent = muzzleobject;
+       // muz.transform.Find("mesh").localRotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        //Destroy(muz.transform.Find("mesh").gameObject, 0.1f);
+        Destroy(muz, 1f);
+        last = cam.GetComponent<MouseLook>().originalRotation;
+        cam.GetComponent<MouseLook>().originalRotation = Quaternion.Euler(Random.Range(-recoil, recoil), Random.Range(-recoil, recoil), Random.Range(-recoil, recoil));
+        rand.localRotation = Quaternion.Euler(0,0, Random.Range(-7, 7));
+
+        if (Physics.Raycast(cam.transform.position,cam.transform.TransformDirection(Vector3.forward), out hit, 100)){
+            if (hit.transform.GetComponent<Rigidbody>()) hit.transform.GetComponent<Rigidbody>().AddForce( -hit.normal * 200);
+            
+        }
+       
+    }
+    public IEnumerator Reload()
+    {
+        reloading = true;
+        //anim.SetTrigger("reload");
+            yield return new WaitForSeconds(reloadTime);
+        reloading = false;
+        curAmmo = maxAmmo;
+    }
+}
---
+++ RECURSE/Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player_Controller : MonoBehaviour
+{
+    [Header("Move Options")]
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float sprintSpeed = 10f;
+    public bool isSprint;
+    [Header("Mouse Options")]
+    public Transform weaponCameraObject;
+    public Transform cameraObject;
+    public Transform weaponTransform;
+    public float weaponSway = 0.8f;
+    public float defaultFOV= 70f;
+    public float sprintFOV= 80f;
+    private float refFloat = 0f;
+    public float speedChangeFOV;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    public float maxRotation;
+    public float rotSpeed;
+    Vector3 refzero = Vector3.zero;
+   // public PostProcessVolume volume;
+   // public ChromaticAberration chromatic;
+   // public Vignette vignette;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        weaponTransform.localEulerAngles = new Vector3(Mathf.SmoothDamp(weaponTransform.rotation.x, cameraObject.rotation.x, ref refFloat, weaponSway), Mathf.SmoothDamp(weaponTransform.rotation.x, cameraObject.rotation.x, ref refFloat, weaponSway), Input.GetAxis("Horizontal") * weaponSway);
+       // weaponTransform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(cameraObject.rotation.x, cameraObject.rotation.y, 0), weaponSway * Time.deltaTime);
+
+        cameraObject.Rotate(Input.GetAxis("Vertical") * -1f, 0, Input.GetAxis("Horizontal") * -1f);
+
+        if (Input.GetAxis("Vertical") > 0.05f || Input.GetAxis("Vertical") < -0.05f  || Input.GetAxis("Horizontal") > 0.05f || Input.GetAxis("Horizontal") < -0.05f)
+        {
+            if(!isSprint) cameraObject.Rotate(0f, 0f, maxRotation * Input.GetAxis("Vertical") * Mathf.Sin(Time.time * rotSpeed));
+            else cameraObject.Rotate(0f, 0f, maxRotation * 1.7f * Input.GetAxis("Vertical") * Mathf.Sin(Time.time * rotSpeed * 2f));
+        }
+        
+        //weaponCameraObject.localEulerAngles = Vector3.SmoothDamp(weaponCameraObject.eulerAngles, cameraObject.eulerAngles, ref refzero, weaponSway);
+        isSprint = Input.GetButton("Sprint");
+        if (isSprint)
+        {
+            cameraObject.GetComponent<Camera>().fieldOfView = Mathf.SmoothDamp(cameraObject.GetComponent<Camera>().fieldOfView, sprintFOV, ref refFloat, speedChangeFOV * Time.deltaTime);
+           // chromatic.intensity.value = Mathf.SmoothDamp(chromatic.intensity, 0.3f, ref refFloat, speedChangeFOV);
+            //vignette.intensity.value = Mathf.SmoothDamp(chromatic.intensity, 0.3f, ref refFloat, speedChangeFOV);
+        }
+        else
+        {
+           // chromatic.intensity.value = Mathf.SmoothDamp(chromatic.intensity, 0.07f, ref refFloat, speedChangeFOV);
+            //vignette.intensity.value = Mathf.SmoothDamp(chromatic.intensity, 0.15f, ref refFloat, speedChangeFOV);
+            cameraObject.GetComponent<Camera>().fieldOfView = Mathf.SmoothDamp(cameraObject.GetComponent<Camera>().fieldOfView, defaultFOV, ref refFloat, speedChangeFOV * Time.deltaTime);
+        }
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            if (!isSprint) moveDirection = moveDirection * speed;
+            else moveDirection = moveDirection * sprintSpeed;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(transform.TransformDirection(moveDirection) * Time.deltaTime);
+    }
+}
